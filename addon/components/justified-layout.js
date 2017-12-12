29375f89898510207343b4cfe61c9d8a7cc97245
Emil Löfquist + Jonas Brusman <dev+emil+jonas@teamtailor.com>
2017-12-12 10:56:13 +0100

    Simple component structure

---
+++ addon/components/justified-layout.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../templates/components/justified-layout';
+
+export default Component.extend({
+  layout
+});
---
+++ app/components/justified-layout.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-justified-layout/components/justified-layout';
