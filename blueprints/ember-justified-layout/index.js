3685e3b16943e2b80df6b2f3c977a175fd2291db
Emil Löfquist + Jonas Brusman <dev+emil+jonas@teamtailor.com>
2017-12-12 15:39:22 +0100

    Expose commonjs packed package to ember
    https://gist.github.com/foxnewsnetwork/276eca2274a328b085ee54926e609cdb

---
+++ blueprints/ember-justified-layout/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  description: 'add justified-layout npm package',
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  afterInstall: function(options) {
+    return this.addPackageToProject('justified-layout', '^2.1.1');
+  },
+};
--- index.js
+++ index.js
@@ -1,6 +1,63 @@
 /* eslint-env node */
 'use strict';
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const Webpack = require('broccoli-webpack');
+const justifiedLayout = new Funnel('node_modules/justified-layout/lib', {
+  destDir: 'justified-layout',
+  files: ['index.js'],
+});
+
+const PackOpts = {
+  entry: 'justified-layout/lib/index.js',
+  output: {
+    filename: 'justified-layout/justified-layout.js',
+    library: 'justified-layout',
+    libraryTarget: 'umd',
+  },
+};
+
+const packedJustifiedLayout = new Webpack([justifiedLayout], PackOpts);
+
+const transformAMD = name => ({
+  using: [{ transformation: 'amd', as: name }],
+});
 
 module.exports = {
-  name: 'ember-justified-layout'
+  name: 'ember-justified-layout',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    while (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    this.app = app;
+
+    const vendor = this.treePaths.vendor;
+    // requires ember-cli 2.9+
+    // https://github.com/ember-cli/ember-cli/pull/5976
+    app.import(
+      vendor + '/justified-layout/justified-layout.js',
+      transformAMD('justified-layout')
+    );
+
+    return app;
+  },
+
+  treeForVendor(vendorTree) {
+    const trees = [packedJustifiedLayout];
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    return mergeTrees(trees);
+  },
+
+  isDevelopingAddon() {
+    return true;
+  },
 };
