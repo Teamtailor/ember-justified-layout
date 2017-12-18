885923da31cbaaaa935b653ebc161f898d4e871c
Jonas Brusman <himynameisjonas@gmail.com>
2017-12-18 10:28:42 +0100

    eslint/prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,24 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  extends: 'eslint:recommended',
+  plugins: ['prettier', 'ember'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
-  }
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    'prettier/prettier': ['error', { trailingComma: 'es5', singleQuote: true }],
+  },
 };
