8923a866d024ab8a3daa0effa78913dc430549e3
Jonas Brusman <himynameisjonas@gmail.com>
2017-12-18 09:25:10 +0100

    Calculate properties for a justified layout

--- addon/components/justified-layout.js
+++ addon/components/justified-layout.js
@@ -1,6 +1,22 @@
 import Component from '@ember/component';
 import layout from '../templates/components/justified-layout';
+import justifiedLayout from 'justified-layout';
+import { computed } from '@ember/object';
+import { get, getProperties } from '@ember/object';
 
 export default Component.extend({
-  layout
+  layout,
+
+  justifiedImages: computed('images.[]', function() {
+    let images = get(this, 'images');
+    let imageSizes = get(this, 'images').map(image => {
+      return getProperties(image, 'width', 'height');
+    });
+    return justifiedLayout(imageSizes).boxes.map((box, index) => {
+      return {
+        image: images.objectAt(index),
+        box,
+      };
+    });
+  }),
 });
