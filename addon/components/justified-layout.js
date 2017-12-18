dbd67986acc5ac112c3fdeeb7599ea8bfba389ab
Emil Löfquist + Jonas Brusman <dev+emil+jonas@teamtailor.com>
2017-12-18 11:24:37 +0100

    Set height after calculation

--- addon/components/justified-layout.js
+++ addon/components/justified-layout.js
@@ -2,10 +2,16 @@ import Component from '@ember/component';
 import layout from '../templates/components/justified-layout';
 import justifiedLayout from 'justified-layout';
 import { computed } from '@ember/object';
-import { get, getProperties } from '@ember/object';
+import { get, set, getProperties } from '@ember/object';
+import { schedule } from '@ember/runloop';
 
 export default Component.extend({
   layout,
+  attributeBindings: ['style'],
+
+  style: computed('height', function() {
+    return `height: ${get(this, 'height')}px;`;
+  }),
 
   justifiedImages: computed('images.[]', function() {
     let images = get(this, 'images');
@@ -13,13 +19,17 @@ export default Component.extend({
       return getProperties(image, 'width', 'height');
     });
 
-    return justifiedLayout(imageSizes, get(this, 'options')).boxes.map(
-      (box, index) => {
-        return {
-          image: images.objectAt(index),
-          box,
-        };
-      }
-    );
+    let calcuations = justifiedLayout(imageSizes, get(this, 'options'));
+
+    schedule('afterRender', () => {
+      set(this, 'height', calcuations.containerHeight);
+    });
+
+    return calcuations.boxes.map((box, index) => {
+      return {
+        image: images.objectAt(index),
+        box,
+      };
+    });
   }),
 });
